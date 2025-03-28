5d60828272f2b0dcaf6378b0ae0616dd655d510c
laidachen <lei949797@gmail.com>
2025-03-28 22:50:31 +0800

    P_8_5

--- P_8_5.cpp
+++ P_8_5.cpp
@@ -1,16 +1,44 @@
 #include <bits/stdc++.h>
+#define N 100001
 using namespace std;
+//設dp[i]為i這節點底下全部的重量，一開始只要先top down把全部節點都算過一遍，之後每次新增物品用dfs探索，然後每經過一個節點都把它加上這物品的重量就好了
+vector<int> dp(2*N,-1);
+vector<int> out[2*N];
+vector<int> in[2*N];
+
+int dpp(int n) {
+	if (dp[n]!=-1) {
+		return dp[n];
+	} else {
+		dp[n] = dpp(out[n][0])+dpp(out[n][1]);
+		return dp[n];
+	}
+}
+
+void dfs(int &n,int dot,int i) {
+	if (dot>=n) {//走到底了
+		dp[dot]+=i;
+		cout<<dot;
+	} else {
+		int l = dp[out[dot][0]];
+		int r = dp[out[dot][1]];
+		if (l<=r) {
+			dp[dot]+=i;
+			dfs(n,out[dot][0],i);
+		} else {
+			dp[dot]+=i;
+			dfs(n,out[dot][1],i);
+		}
+	}
+}
 
 int main(void) {
 	ios::sync_with_stdio(0),cin.tie(0);
 	int n,m,temp,p,s,t;
 	cin>>n>>m;
-	vector<int> weight(2*n,0);
-	vector<int> out[2*n];
-	vector<int> in[2*n];
 	vector<int> command;
 	for (int i=n; i<=2*n-1; i++) {
-		cin>>weight[i];
+		cin>>dp[i];
 	}
 	for (int i=0; i<m; i++) {
 		cin>>temp;
@@ -20,10 +48,13 @@ int main(void) {
 		cin>>p>>s>>t;
 		out[p].push_back(s);
 		out[p].push_back(t);
-		in[s].push_back(p);
-		in[t].push_back(p);
 	}
-
-	//top down
+	for (int i=0; i<command.size(); i++) {
+		dpp(1);
+		dfs(n,1,command[i]);
+		if (i!=command.size()-1) {
+			cout<<" ";
+		}
+	}
 	return 0;
 }
